0d417433f6ec493e400e394071afe074c19edb6a
Maxwell Elliot Heiber <mheiber@fb.com>
2021-11-20 19:46:17 +0000

    WIP

--- keyboards/lily58/keymaps/maxwell/keymap.c
+++ keyboards/lily58/keymaps/maxwell/keymap.c
@@ -48,7 +48,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-[_LOWER] = LAYOUT(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL, KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL, KC_NO, KC_TRNS, KC_GRV, KC_TILD, KC_UNDS, KC_PLUS, KC_PEQL, KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, MO(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS),
+[_LOWER] = LAYOUT(
+  KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                       KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL,
+  KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5,                                            KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, KC_TRNS,
+  KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                                         KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL, KC_NO,
+  KC_TRNS, KC_GRV, KC_TILD, KC_UNDS, KC_PLUS, KC_PEQL,   KC_LPRN,    KC_RPRN,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
+  KC_TRNS, KC_TRNS, MO(3), KC_TRNS,                                                 KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
@@ -56,15 +61,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------|      | Left | Down |  Up  |Right |      |
- * |------+------+------+------+------+-71Gjjjj-----|   [   |    |    ]  |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-
-[_RAISE] = LAYOUT(KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_WH_U, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_WH_L, KC_WH_D, KC_WH_R, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, SGUI(KC_LBRC), SGUI(KC_RBRC), KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+	[_RAISE] = LAYOUT(
+  KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
+  KC_TRNS, KC_NO, KC_NO, KC_WH_U, KC_NO, KC_NO,                                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
+  KC_TRNS, KC_NO, KC_WH_L, KC_WH_D, KC_WH_R, KC_NO,                                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
+  KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     SGUI(KC_LBRC), SGUI(KC_RBRC),     KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -89,6 +98,14 @@ const char *read_keylogs(void);
 
 // const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
+// void set_timelog(void);
+// const char *read_timelog(void);
+
+void oled_task_user(void) {
+  if (is_keyboard_master()) {
+    // If you want to change the display of OLED, you need to change here
+    oled_write_ln(read_layer_state(), false);
+    oled_write_ln(read_keylog(), false);
     oled_write_ln(read_keylogs(), false);
     //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
     //oled_write_ln(read_host_led_state(), false);

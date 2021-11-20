eb511fee3fe9b1a38c9a89adf5ce87fdee66a6f2
Maxwell Elliot Heiber <mheiber@fb.com>
2021-11-20 20:05:46 +0000

    good

--- keyboards/lily58/keymaps/maxwell/keymap.c
+++ keyboards/lily58/keymaps/maxwell/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                        KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,   KC_LBRC, KC_RBRC,    KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
 
-    KC_LALT, KC_LGUI, MO(1),           KC_ENT,                 KC_SPC, MO(2), KC_BSPC, LCTL(KC_B)),
+    KC_LALT, KC_LGUI, MO(_LOWER),           KC_ENT,                 KC_SPC, KC_Z/*MO(2)*/, KC_BSPC, MO(_RAISE)),//LCTL(KC_B)),
 
 
 /* LOWER
@@ -70,8 +70,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 	[_RAISE] = LAYOUT(
   KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-  KC_TRNS, KC_NO, KC_NO, KC_WH_U, KC_NO, KC_NO,                                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-  KC_TRNS, KC_NO, KC_WH_L, KC_WH_D, KC_WH_R, KC_NO,                                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
+  KC_TRNS, KC_NO, KC_NO, KC_WH_U, KC_NO, KC_NO,                                     KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_TRNS,
+  KC_TRNS, KC_NO, KC_WH_L, KC_WH_D, KC_WH_R, KC_NO,                                 KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_TRNS,
   KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     SGUI(KC_LBRC), SGUI(KC_RBRC),     KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };

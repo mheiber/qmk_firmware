5d453358dfaf89d50d90d37d46ff029b5d157dae
Maxwell Elliot Heiber <mheiber@fb.com>
2022-02-13 21:37:43 +0000

    adjust

--- keyboards/lily58/keymaps/maxwell/keymap.c
+++ keyboards/lily58/keymaps/maxwell/keymap.c
@@ -8,7 +8,7 @@ enum layer_number {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
+  
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
@@ -24,12 +24,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
  [_QWERTY] = LAYOUT( \
-    KC_ESC, LGUI(KC_GRV), SGUI(KC_LBRC), SGUI(KC_RBRC), KC_SPC,          LAG(KC_0), LAG(KC_1), LAG(KC_2), MO(_LOWER), KC_DEL, KC_ENT, KC_BSPC, \
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, \
-    KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                        KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,   KC_LCBR, KC_RCBR,    KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
+    KC_ESC, LGUI(KC_GRV), SGUI(KC_LBRC), SGUI(KC_RBRC), KC_SPC, MO(_ADJUST),   LAG(KC_0), LAG(KC_1), LAG(KC_2), KC_DEL, KC_ENT, KC_BSPC, \
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                      KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, \
+    KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                                     KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,   KC_LCBR,                KC_RCBR,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
 
-    KC_LALT, KC_LGUI, MO(_LOWER),           KC_ENT,                 KC_SPC, MO(_RAISE), KC_BSPC, LCTL(KC_B)),
+    KC_LALT, KC_LGUI, MO(_LOWER),           KC_ENT,                        KC_SPC, MO(_RAISE), KC_BSPC, LCTL(KC_B)),
 
 
 /* LOWER
@@ -67,23 +67,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 	[_RAISE] = LAYOUT(
-  KC_TRNS, KC_F11, KC_F10, KC_NO, KC_NO, KC_NO,                                       KC_MPLY, KC_MRWD, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU,
+  KC_TRNS, KC_F11, KC_F10, KC_NO, KC_NO, KC_TRNS,                                       KC_MPLY, KC_MRWD, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU,
   KC_TRNS, KC_NO, KC_WH_D, KC_MS_U, KC_WH_U, KC_NO,                                     KC_PGUP, KC_HOME, KC_UP, KC_END, KC_ACL1, KC_0,
   KC_TRNS, KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R,                                 KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_ACL2, KC_TRNS,
   KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_BTN2, KC_BTN1,     KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-    // [_ADJUST] = LAYOUT(
-  // KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-  // KC_TRNS, KC_NO, KC_NO, KC_WH_U, KC_NO, KC_NO,                                     KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_TRNS,
-  // KC_TRNS, KC_NO, KC_WH_L, KC_WH_D, KC_WH_R, KC_NO,                                 KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_TRNS,
-  // KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     SGUI(KC_LBRC), SGUI(KC_RBRC),     KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS,
-  // KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+    [_ADJUST] = LAYOUT(
+  KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,                                       KC_TRNS, KC_TRNS, KC_PSLS, KC_PAST, KC_PMNS, KC_TRNS,
+  KC_TRNS, KC_NO, KC_NO, KC_WH_U, KC_NO, KC_NO,                                     KC_NO,   KC_P7,   KC_P8,   KC_P9, KC_PPLS, KC_TRNS,
+  KC_TRNS, KC_NO, KC_WH_L, KC_WH_D, KC_WH_R, KC_NO,                                 KC_NO,   KC_P4,   KC_P5,   KC_P6, KC_PCMM, KC_TRNS,
+  KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     SGUI(KC_LBRC), SGUI(KC_RBRC),     KC_NO,   KC_P1,   KC_P2,   KC_P3, KC_PEQL, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                       KC_TRNS,      KC_P0, KC_TRNS, KC_PDOT),
 
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  return state;
+  // return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
